1703c0aee2beea9177d1f70a5a683b62a68ebb84
Pedro Jesus Ibarra Palmero <52764875+PedroJesus044@users.noreply.github.com>
2024-07-22 10:59:16 -0600

    Update config.inc.php

--- phpMyAdmin/config.inc.php
+++ phpMyAdmin/config.inc.php
@@ -27,7 +27,7 @@ $i++;
 /* Authentication type */
 $cfg['Servers'][$i]['auth_type'] = 'cookie';
 /* Server parameters */
-$cfg['Servers'][$i]['host'] = 'mariadb';
+$cfg['Servers'][$i]['host'] = 'mariadbabkp';
 $cfg['Servers'][$i]['compress'] = false;
 $cfg['Servers'][$i]['AllowNoPassword'] = false;
 
